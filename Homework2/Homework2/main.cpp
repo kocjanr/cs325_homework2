73bb8e2c3209f68d71efaa3e34c53dc25c184e1b
Ryan Kojan <ryankojan@Ryans-MacBook-Pro.local>
2017-01-25 18:29:58 -0800

    Setup
    New Files

---
+++ Homework2/Homework2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework2
+//
+//  Created by Ryan Kojan on 1/25/17.
+//  Copyright © 2017 Ryan Kojan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
